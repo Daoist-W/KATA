1e2f4e66e586f3341cc3867c4be83c7c3066e39d
donpachi <isikodon@googlemail.com>
2022-01-07 10:31:18 +0000

    adding ArrayList exercise

---
+++ QA/Arrays/ArrayLists.java
@@ -0,0 +1,45 @@
+
+class ArrayLists {
+	
+	public static void main(String[] args) {
+		ArrayList<String> myArray = new ArrayList<>();
+
+		myArray.add("Hello1");
+		myArray.add("Hello2");
+		myArray.add("Hello3");
+		myArray.add("Hello4");
+		myArray.add("Hello5");
+		myArray.add("Hello6");
+
+
+		System.out.println(myArray);
+
+		for(String element : myArray) {
+			System.out.println(element);
+		}
+
+
+		myArray.get(0);
+
+		myArray.set(3m "hola");
+
+		myArray.remove(2);
+
+
+		Collections.sort(myArray);
+
+		Collections.reverse(myArray);
+
+		Collections.swap(myArray, 1, 2);
+
+
+		ArrayList<String> clonedArray = new ArrayList<>();
+
+		clonedArray = (ArrayList<String>) myArray.clone();
+
+
+
+
+
+	}	
+}
